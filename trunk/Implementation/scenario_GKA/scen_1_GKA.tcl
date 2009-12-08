f3ebdd562e2fff8b56dd22940d642b7481a01f8c
gimric <gimric@08ab9078-a752-11de-ba6e-69e41c8db662>
2009-12-08 08:40:08 +0000

    GKA scenario mods.

--- trunk/Implementation/scenario_GKA/scen_1_GKA.tcl
+++ trunk/Implementation/scenario_GKA/scen_1_GKA.tcl
@@ -10,20 +10,34 @@ proc finish {} {
         exit 0
 }
 
-#Topology Configuration
+# Topology Configuration
 
-for {set i 0} {$i < 10} {incr i} {
+for {set i 0} {$i < 20} {incr i} {
   set n($i) [$ns node]
   set m($i) [$ns node]
 }
 
-for {set i 0} {$i < 9} {incr i} {
-  $ns duplex-link $n($i) $n([expr ($i+1)]) 1Mb 10ms DropTail
-  $ns duplex-link $m($i) $m([expr ($i+1)]) 1Mb 10ms DropTail
+$ns duplex-link $n(0) $n(1) 1Mb 10ms DropTail
+$ns duplex-link $n(0) $n(2) 1Mb 10ms DropTail
+
+$ns duplex-link $m(0) $m(1) 1Mb 10ms DropTail
+$ns duplex-link $m(0) $m(2) 1Mb 10ms DropTail
+
+for {set i 2} {$i < 9} {incr i} {
+  $ns duplex-link $n(1) $n([expr ($i+1)]) 1Mb 10ms DropTail
+  $ns duplex-link $m(1) $m([expr ($i+1)]) 1Mb 10ms DropTail
 }
+
+for {set i 9} {$i < 19} {incr i} {
+  $ns duplex-link $n(2) $n([expr ($i+1)]) 1Mb 10ms DropTail
+  $ns duplex-link $m(2) $m([expr ($i+1)]) 1Mb 10ms DropTail
+}
+
+# link between leaders
 $ns duplex-link $m(0) $n(0) 1Mb 10ms DropTail
 
-#Create a GKA agent and attach it to node n0
+
+# Create GKA agents and attach one of them to each node
 
 for {set i 0} {$i < 10} {incr i} {
   set gka_n($i) [new Agent/GKA]
@@ -34,6 +48,9 @@ for {set i 0} {$i < 10} {incr i} {
   $ns attach-agent $n($i) $gka_n($i)
   $ns attach-agent $m($i) $gka_m($i)
 }
+
+
+# Simulation 
 
 for {set i 0} {$i < 9} {incr i} {
 $ns at [expr (0.5+$i*0.5)] "$gka_n($i) join $gka_n(9)"
